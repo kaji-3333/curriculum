0bf6d9090e1dd3bcf913b897f4eaff9bd285d9be
kaji-3333 <79329174+kaji-3333@users.noreply.github.com>
2021-02-23 01:57:30 +0900

    課題提出テスト
    Task1_1.javaの提出

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,25 @@
+public class Task1_1 {
+    
+    public static void main(String[] args) {
+       
+        int hogeInt = 50;
+
+        String hogeString = "テスト";
+
+        //[浮動小数点型のdouble型の　hogeDouble という変数を宣言し、値「0.2」で初期化している]
+        double hogeDouble = 0.2;
+
+        hogeInt = 100;
+
+        hogeString = "合格";
+
+        System.out.println(hogeInt);
+        
+        //「変数　hogeString の値を表示している」
+        System.out.println(hogeString);
+
+        System.out.println(hogeDouble);
+
+
+    }
+}
